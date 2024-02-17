1c48483dae3431f47f8022d42deca5e8351bc403
nastarandehnavi <nastaran23000@student.hamk.fi>
2024-02-17 23:45:30 +0200

    ex7

--- Read.php
+++ Read.php
@@ -1,28 +1,68 @@
 <?php 
-   $title = "your information";
+   $title = "read your data";
    
    include 'header.php' ?>
 
 <?php
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    <?php
+    include 'db.php';
+    // SQL query to retrieve data from the 'studentsinfo' table
+    $sql = "SELECT * FROM customers";
     
+    // Execute the SQL query and store the result
+    $result = $conn->query($sql);
+    
+    // Check if there are any results
+    if ($result->num_rows > 0) {
+        echo "<table class='table'>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>first_name</th>
+                        <th>last_name</th>
+                        <th>city</th>
+                
+                    </tr>
+                </thead>
+                <tbody>";
+    
+        // Loop through the result set and display data in rows
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>
+                    <td>{$row['ID']}</td>
+                    <td>{$row['first_name']}</td>
+                    <td>{$row['last_name']}</td>
+                    <td>{$row['city']}</td>
+                    
+                  </tr>";
+        }
+    
+        echo "</tbody></table>";
+    } else {
+        // Display a message if no results are found
+        echo "No results";
+    }
+    // close the connection when done
+    $conn->close();
+    ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <?php include 'footer.php' ?>

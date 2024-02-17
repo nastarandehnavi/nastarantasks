1856432c3659e64f39d64f006a5f7fffa3f43b5d
nastarandehnavi <nastaran23000@student.hamk.fi>
2024-02-18 01:19:02 +0200

    ex7

--- updatesingle.php
+++ updatesingle.php
@@ -1,57 +1,70 @@
 <?php
-$title = "updating";
+$title = "Exercise 7";
 include 'header.php';
 
+// Include database connection
 include 'nd.php';
-$a = $_GET['id'];
-$result = mysqli_query($conn,"SELECT * FROM studentsinfo WHERE id= '$a'");
-$row = mysqli_fetch_array($result);
+
+// Check if form is submitted
+if(isset($_POST['submit'])) {
+    // Get form data
+    $id = $_POST['Id'];
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $city = $_POST['city'];
+
+    // SQL to update record
+    $update_query = "UPDATE studentsinfo SET first_name='$first_name', last_name='$last_name', city='$city' WHERE id='$id'";
+    
+    // Perform update
+    if(mysqli_query($conn, $update_query)) {
+        echo "<div class='alert alert-success'>Record updated successfully</div>";
+    } else {
+        echo "<div class='alert alert-danger'>Error updating record: " . mysqli_error($conn) . "</div>";
+    }
+}
+
+// Check if delete button is clicked
+if(isset($_POST['delete'])) {
+    // Get ID from form
+    $id = $_POST['ID'];
+
+    // SQL to delete record
+    $delete_query = "DELETE FROM studentsinfo WHERE id='$id'";
+    
+    // Perform deletion
+    if(mysqli_query($conn, $delete_query)) {
+        echo "<div class='alert alert-success'>Record deleted successfully</div>";
+    } else {
+        echo "<div class='alert alert-danger'>Error deleting record: " . mysqli_error($conn) . "</div>";
+    }
+}
+
+// Close database connection
+mysqli_close($conn);
 ?>
 
-<h2> Update your information below: </h2>
-<form name="form1" method="post" action="">
-  <div class="row">
-    <div class="col">
-      <input type="text" class="form-control" placeholder="first_name" name="first_name" required value="<?php echo $row['first_name']; ?>">
+<h2>Input your information</h2>
+
+<form method="post" action="">
+    <div class="form-group">
+        <label for="ID">ID:</label>
+        <input type="number" class="form-control" id="ID" name="ID" required>
     </div>
-    <div class="col">
-      <input type="text" class="form-control" placeholder="last_name" name="last_name" required value="<?php echo $row['last_name']; ?>" >
+    <div class="form-group">
+        <label for="first_name">First Name:</label>
+        <input type="text" class="form-control" id="first_name" name="first_name" required>
     </div>
-  </div>
-  <br>
-  <div class="row">
-    <div class="col">
-      <input type="text" class="form-control" placeholder="city" name="city" required value="<?php echo $row['city']; ?>">
+    <div class="form-group">
+        <label for="last_name">Last Name:</label>
+        <input type="text" class="form-control" id="last_name" name="last_name" required>
     </div>
-  </div>
-  <br>
-  <div class="row">
-    <div class="col"><button type="submit" class="btn btn-primary" name="submit">Update your Information</button></div>
-    <div class="col"><button type="submit" class="btn btn-primary" name="delete">Delete your Information</button></div>
-  </div>
+    <div class="form-group">
+        <label for="city">City:</label>
+        <input type="text" class="form-control" id="city" name="city" required>
+    </div>
+    <button type="submit" class="btn btn-primary" name="submit">Update</button>
+    <button type="submit" class="btn btn-danger" name="delete">Delete</button>
 </form>
 
-<?php 
-if (isset($_POST['submit'])) {
-    $fname = $_POST['first_name'];
-    $lname = $_POST['last_name'];
-    $query = mysqli_query($conn, "UPDATE studentsinfo SET first_name='$fname', last_name='$lname' WHERE id='$a'");
-    if ($query) {
-        echo "<h2>Your information is updated Successfully</h2>";
-    } else {
-        echo "Record Not modified";
-    }
-}
-
-if (isset($_POST['delete'])) {
-    $query = mysqli_query($conn, "DELETE FROM studentsinfo WHERE id='$a'");
-    if ($query) {
-        echo "Record Deleted with id: $a <br>";
-    } else {
-        echo "Record Not Deleted";
-    }
-}
-
-$conn->close();
-include 'footer.php';
-?>
+<?php include 'footer.php'; ?>

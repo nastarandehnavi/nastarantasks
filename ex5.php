ecf22f5cb5e2a5045372934972416dbdb17bf857
nastarandehnavi <nastaran23000@student.hamk.fi>
2024-02-17 20:36:41 +0200

    ex5

---
+++ ex5.php
@@ -0,0 +1,17 @@
+<?php 
+   $title = "Exercise 5";
+   include 'header.php'; 
+?>
+
+<style>
+ 
+  
+  .nav-link {
+    color: black; /* Set the color of the link to black */
+    /* Your styles for nav-link */
+  }
+</style>
+
+ <li><a class="nav-link" href="https://github.com/POORY-AZARE/php.teamwork/blob/main/README.md">README.md</a> </li>
+
+<?php include 'footer.php'; ?>

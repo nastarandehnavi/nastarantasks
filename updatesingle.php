a97db841dc1d5686acc5267371ffb007672f1626
nastarandehnavi <nastaran23000@student.hamk.fi>
2024-02-18 01:16:12 +0200

    ex7

--- updatesingle.php
+++ updatesingle.php
@@ -1,21 +1,15 @@
 <?php
 $title = "updating";
+include 'header.php';
 
-
-include 'header.php' ?>
-
-
-
-
-
-<?php
-include 'ex7.php';
+include 'nd.php';
 $a = $_GET['id'];
-$result = mysqli_query($conn,"SELECT * FROM customers WHERE id= '$a'");
-$row= mysqli_fetch_array($result);
+$result = mysqli_query($conn,"SELECT * FROM studentsinfo WHERE id= '$a'");
+$row = mysqli_fetch_array($result);
 ?>
+
 <h2> Update your information below: </h2>
-<form name= "form1" method="post" action="ex7.php">
+<form name="form1" method="post" action="">
   <div class="row">
     <div class="col">
       <input type="text" class="form-control" placeholder="first_name" name="first_name" required value="<?php echo $row['first_name']; ?>">
@@ -29,46 +23,35 @@ $row= mysqli_fetch_array($result);
     <div class="col">
       <input type="text" class="form-control" placeholder="city" name="city" required value="<?php echo $row['city']; ?>">
     </div>
-
-   
-<br>
+  </div>
+  <br>
   <div class="row">
-  <div class="col"><button type="submit" class="btn btn-primary" name="submit">Update your Information</button></div>
-  <div class="col"><button type="submit" class="btn btn-primary" name="delete">Delete your Information</button></div>
-</div>
+    <div class="col"><button type="submit" class="btn btn-primary" name="submit">Update your Information</button></div>
+    <div class="col"><button type="submit" class="btn btn-primary" name="delete">Delete your Information</button></div>
+  </div>
 </form>
+
 <?php 
-/* 
-The isset() function is used to check if a variable is set and not NULL.
- In this case, it's checking if the $_POST['submit'] 
-value is set and not NULL. If the form has been submitted, the value of $_POST['submit'] will be set,
-and the code inside the if block will be executed. If the form has not been submitted, 
-the value of $_POST['submit'] will not be set, and the code inside the if block will not be executed.
-*/
-if (isset($_POST['submit'])){
-    
+if (isset($_POST['submit'])) {
     $fname = $_POST['first_name'];
     $lname = $_POST['last_name'];
-    $query = mysqli_query($conn,"UPDATE studentsinfo set first_name='$first_name', last_name='$last_name' where id='$a'");
-    if($query){
+    $query = mysqli_query($conn, "UPDATE studentsinfo SET first_name='$fname', last_name='$lname' WHERE id='$a'");
+    if ($query) {
         echo "<h2>Your information is updated Successfully</h2>";
-        // if you want to redirect to update page after updating
+    } else {
+        echo "Record Not modified";
     }
-    else { echo "Record Not modified";}
-    }
+}
 
-    if (isset($_POST['delete'])){
-        $query = mysqli_query($conn,"DELETE FROM studentsinfo where id='$a'");
-        if($query){
-            echo "Record Deleted with id: $a <br>";
-            // if you want to redirect to update page after updating
-            //header("location: update.php");
-        }
-        else { echo "Record Not Deleted";}
-        }
+if (isset($_POST['delete'])) {
+    $query = mysqli_query($conn, "DELETE FROM studentsinfo WHERE id='$a'");
+    if ($query) {
+        echo "Record Deleted with id: $a <br>";
+    } else {
+        echo "Record Not Deleted";
+    }
+}
 
 $conn->close();
-
+include 'footer.php';
 ?>
-<?
-include 'footer.php' ?>

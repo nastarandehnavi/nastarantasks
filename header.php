16e2371a37cc750e620c83c7c66ea0f660494a6b
nastarandehnavi <nastaran23000@student.hamk.fi>
2024-02-17 20:37:43 +0200

    ex5

--- header.php
+++ header.php
@@ -39,7 +39,7 @@
           <a class="nav-link" href="ex4.php">Exercise 4</a>
           </li>
          <li class="nav-item">
-          <a class="nav-link" href="#">Exercise 5</a>
+          <a class="nav-link" href="ex5.php">Exercise 5</a>
          </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Exercise 6</a>

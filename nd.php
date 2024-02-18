80e22cfca65cbbf697f1604cb2e88953469c1249
nastarandehnavi <nastaran23000@student.hamk.fi>
2024-02-18 04:19:09 +0200

    ex7

--- nd.php
+++ nd.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost"; // Replace with your MySQL server hostname
-$username = "nastaran23000";     // Replace with your MySQL username
-$password = "R5OEMKLK ";     // Replace with your MySQL password
-$dbname = "wp_nastaran23000";       // Replace with the name of your MySQL database
+$servername = "localhost"; 
+$username = "nastaran23000";     
+$password = "R5OEMKLK";     
+$dbname = "wp_nastaran23000";      
 
 // Create a database connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,4 +11,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 ?>

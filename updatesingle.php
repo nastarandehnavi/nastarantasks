1c59ec513be0a01142c5dbdcb96c855392ddee46
nastarandehnavi <nastaran23000@student.hamk.fi>
2024-02-18 02:13:20 +0200

    ex7

--- updatesingle.php
+++ updatesingle.php
@@ -1,20 +1,20 @@
+
 <?php
 $title = "update";
-include 'header.php';
 
+
+include 'header.php' ?>
+
+
+
+<?php
 include 'nd.php';
-
-// Check if ID is provided in the URL
-if(isset($_GET['Id'])) {
-    $a = $_GET['Id'];
-    
-    // Fetch data for the provided ID
-    $result = mysqli_query($conn,"SELECT * FROM customers WHERE Id= '$a'");
-    $row = mysqli_fetch_array($result);
+$a = $_GET['Id'];
+$result = mysqli_query($conn,"SELECT * FROM customers WHERE Id= '$a'");
+$row= mysqli_fetch_array($result);
 ?>
-
 <h2> Update your information below: </h2>
-<form name="form1" method="post" action="">
+<form name= "form1" method="post" action="">
   <div class="row">
     <div class="col">
       <input type="text" class="form-control" placeholder="first_name" name="first_name" required value="<?php echo $row['first_name']; ?>">
@@ -28,39 +28,45 @@ if(isset($_GET['Id'])) {
     <div class="col">
       <input type="text" class="form-control" placeholder="city" name="city" required value="<?php echo $row['city']; ?>">
     </div>
-  </div>
-  <br>
+
+    
+<br>
   <div class="row">
-    <div class="col"><button type="submit" class="btn btn-primary" name="submit">Update your Information</button></div>
-    <div class="col"><button type="submit" class="btn btn-primary" name="delete">Delete your Information</button></div>
-  </div>
+  <div class="col"><button type="submit" class="btn btn-primary" name="submit">Update your Information</button></div>
+  <div class="col"><button type="submit" class="btn btn-primary" name="delete">Delete your Information</button></div>
+</div>
 </form>
-
-<?php
-    // Handle form submissions
-    if(isset($_POST['submit'])) {
-        $fname = $_POST['first_name'];
-        $lname = $_POST['last_name'];
-        // Use $fname and $lname variables instead of $first_name and $last_name
-        $query = mysqli_query($conn,"UPDATE customers set first_name='$fname', last_name='$lname' where Id='$a'");
-        if($query) {
-            echo "<h2>Your information is updated Successfully</h2>";
-        } else {
-            echo "Record Not modified";
-        }
+<?php 
+/* 
+The isset() function is used to check if a variable is set and not NULL.
+ In this case, it's checking if the $_POST['submit'] 
+value is set and not NULL. If the form has been submitted, the value of $_POST['submit'] will be set,
+and the code inside the if block will be executed. If the form has not been submitted, 
+the value of $_POST['submit'] will not be set, and the code inside the if block will not be executed.
+*/
+if (isset($_POST['submit'])){
+    
+    $fname = $_POST['first_name'];
+    $lname = $_POST['last_name'];
+    $query = mysqli_query($conn,"UPDATE customers set first_name='$first_name', last_name='$last_name' where Id='$a'");
+    if($query){
+        echo "<h2>Your information is updated Successfully</h2>";
+        // if you want to redirect to update page after updating
+    }
+    else { echo "Record Not modified";}
     }
 
-    if(isset($_POST['delete'])) {
-        // Handle delete operation
+    if (isset($_POST['delete'])){
         $query = mysqli_query($conn,"DELETE FROM customers where Id='$a'");
-        if($query) {
+        if($query){
             echo "Record Deleted with Id: $a <br>";
-        } else {
-            echo "Record Not Deleted";
+            // if you want to redirect to update page after updating
+            //header("location: update.php");
         }
-    }
-}
+        else { echo "Record Not Deleted";}
+        }
 
 $conn->close();
-include 'footer.php';
+
 ?>
+  <?  include footer.php ?>
